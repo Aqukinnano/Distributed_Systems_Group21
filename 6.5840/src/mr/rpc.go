11dbe925b2bea1cc4d1d325b6cac15c19f3f7d49
Mayoi <85593822+Aqukinnano@users.noreply.github.com>
2024-12-02 18:55:01 +0800

    Update rpc.go
    add a few RPC definitions

--- 6.5840/src/mr/rpc.go
+++ 6.5840/src/mr/rpc.go
@@ -37,6 +37,7 @@ type TaskRequest struct {
 	WorkerId    int
 	FileName    string
 	ReduceId    int
+	MapId       int // add MapId
 }
 
 type TaskResponse struct {
@@ -47,9 +48,28 @@ type TaskResponse struct {
 	MapCount int
 	NReduce  int
 	AllDone  bool
+	Data     []byte // add Data
 }
 
 // Add your RPC definitions here.
+type SendFileArgs struct {
+	MapId    int
+	ReduceId int
+	Data     []byte
+}
+
+type SendFileReply struct {
+	Success bool
+}
+
+type FetchReduceInputArgs struct {
+	ReduceId int
+	MapId    int
+}
+
+type FetchReduceInputReply struct {
+	Data []byte
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
